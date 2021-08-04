7b31062ef85a8aa93f428dcb93bbe00561dcf2e8
Nab69 <17692533+Nab69@users.noreply.github.com>
2021-08-04 17:06:48 +0200

    controller & actionfilter

--- WebApp/Controllers/ClassroomController.cs
+++ WebApp/Controllers/ClassroomController.cs
@@ -1,10 +1,12 @@
 ﻿using Dal;
 using DomainModel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApp.Filters;
 
 namespace WebApp.Controllers
 {
@@ -26,6 +28,7 @@ namespace WebApp.Controllers
         }
 
         // GET: Classroom/Details/5
+        [Log]
         public IActionResult Details(int? id)
         {
             if (id == null)
@@ -35,6 +38,9 @@ namespace WebApp.Controllers
 
             if (classroom == null)
                 return NotFound();
+
+            //ViewData["Title"] = "Hello world";
+            //ViewBag.Title = "Hello world";
 
             return View(classroom);
         }
@@ -48,9 +54,50 @@ namespace WebApp.Controllers
 
         // POST: Classroom/Create
         [HttpPost]
-        public IActionResult Create(string test)
+        public IActionResult Create(Classroom classroom)
         {
-            return View();
+            if (this.ModelState.IsValid)
+            {
+                this.context.Classrooms.Add(classroom);
+                this.context.SaveChanges();
+                return RedirectToAction(nameof(Details), new { id = classroom.ClassroomId });
+            }
+
+            return View(classroom);
+        }
+
+        // GET: Classroom/Delete
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+
+            Classroom classroom = this.context.Classrooms.Find(id);
+
+            if (classroom == null)
+                return NotFound();
+
+            return View(classroom);
+        }
+
+        // POST: Classroom/Delete
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+
+            Classroom classroom = this.context.Classrooms.Find(id);
+
+            if (classroom == null)
+                return NotFound();
+
+            this.context.Classrooms.Remove(classroom);
+            this.context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ WebApp/Filters/LogAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Filters
+{
+    public class LogAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Debug.WriteLine("Avant l'action");
+
+            base.OnActionExecuting(filterContext);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            Debug.WriteLine("Après l'action");
+
+            base.OnActionExecuted(context);
+        }
+    }
+
+}
